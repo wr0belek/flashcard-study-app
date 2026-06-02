a5bb8c30ac733089e178431cd8095da76eb21994
Małgorzata Wróbel <mal07gosia@gmail.com>
2026-06-02 21:35:44 +0200

    Add setWeight and getWeight methods to Flashcard

--- Flashcard.java
+++ Flashcard.java
@@ -7,11 +7,25 @@ public class Flashcard
     {
         this.question = q;
         this.answer = a;
-        this.weight = w;
+        this.setWeight(w);
     }
     public String toString() 
     {
         return question + " - " + answer + "(" + weight + ")";
+    }
+    public int getWeight()
+    {
+        return this.weight;
+    }
+    public void setWeight(int w)
+    {
+        if(1 <= w && w <= 5)
+            this.weight = w;
+        else
+        {
+            this.weight = 1;
+            System.out.println("Weight must be between 1 and 5. Setting weight to 1.");
+        }
     }
     public Boolean isCorrent(String users_answer)
     {
