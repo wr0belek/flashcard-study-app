e5ddf100f212464aea2529b140657ec7e0265702
Małgorzata Wróbel <mal07gosia@gmail.com>
2026-06-10 23:00:06 +0200

    Add decks getter to Library class

--- src/Library.java
+++ src/Library.java
@@ -2,7 +2,11 @@ import java.util.ArrayList;
 public class Library
 {
     ArrayList<Deck> decks;
-    public void add_deck(String name)
+    public ArrayList<Deck> getDecks()
+    {
+        return this.decks;
+    }
+    public void addDeck(String name)
     {
         Deck new_deck = new Deck(name);
         decks.add(new_deck);
