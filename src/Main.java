23f6f9eaa748b6cb6774358eef4a9f80b2e69da3
Małgorzata Wróbel <mal07gosia@gmail.com>
2026-06-13 14:07:11 +0200

    Complete Session's arguments

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
+import java.util.Random;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args)
     {
         System.out.println("Welcome to my flashcard study app!");
         Scanner scanner = new Scanner(System.in);
-        Session study_session = new Session(scanner);
+        Library library = FileStorage.readFromFile();
+        Random rand = new Random();
+        Session study_session = new Session(scanner, library, rand);
+        study_session.study();
     }
 }
